b9da09cd8be82949ca7d43b028f1ff88bd35aaf4
Matthew Yu <matthewyu@Matthews-MacBook-Pro-583.local>
2026-06-29 20:36:04 -0400

    Initial commit

---
+++ HeroData.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var hero_name: String
+var hero_class: String        # "Knight", "Mage", etc.
+var power: int
+var weekly_upkeep: int
+var affinity: Dictionary      # { other_hero_id: int }  ← Phase 2
---
+++ ItemData.gd
@@ -0,0 +1,5 @@
+extends Node
+var item_name: String
+var cost: int
+var power_bonus: int
+var equipped_to: String       # hero id, or ""
